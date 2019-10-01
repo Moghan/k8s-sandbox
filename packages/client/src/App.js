a6c3d1a07e2eef4a899314a9f173be14fe11f007
Anders Kensby <anders.kensby@gmail.com>
2019-10-01 22:38:49 +0200

    dice

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import logo from './logo.svg';
 import './App.css';
+import dice from './utils/dice';
 
 import MyForm from './components/my-form';
 
 function App() {
+  dice();
   return (
     <div className="App">
       <MyForm />      
